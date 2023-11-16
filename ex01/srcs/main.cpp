36ee2956bc0353f6f8ebd56c8f5f2483ba0ced99
ryhara <ryosei.hara@gmail.com>
2023-11-16 10:58:02 +0900

    [Add] ex01 test

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -49,7 +49,6 @@ int main(void)
 		std::cout << "------ 10000 numbers test -----" << std::endl;
 		Span span(10000);
 		std::vector<int> v;
-		std::srand(std::time(NULL));
 		for (int i = 0; i < 10000; i++)
 			v.push_back(i);
 		try {
@@ -67,12 +66,40 @@ int main(void)
 			std::cout << RED << e.what() << END << std::endl;
 		}
 	}
-
 	{
-		std::cout << "----- empty span test -----" << std::endl;
+		std::cout << "------ 10000 random numbers test -----" << std::endl;
+		Span span(10000);
+		std::vector<int> v;
+		std::srand(std::time(NULL));
+		for (int i = 0; i < 10000; i++)
+			v.push_back(std::rand());
+		try {
+			std::cout << "span.addNumber(v.begin(), v.end()): " << std::endl;
+			span.addNumber(v.begin(), v.end());
+			std::cout << "min element: " << *(std::min_element(span.begin(), span.end())) << std::endl;
+			std::cout << "max element: " << *(std::max_element(span.begin(), span.end())) << std::endl;
+			std::cout << "span.shortestSpan(): " << span.shortestSpan() << std::endl;
+			std::cout << "span.longestSpan(): " << span.longestSpan() << std::endl;
+			std::cout << "span.addNumber(1);" << std::endl;
+			span.addNumber(1);
+		} catch (std::exception &e) {
+			std::cout << RED << e.what() << END << std::endl;
+		}
+	}
+	{
+		std::cout << "----- empty or one element span test -----" << std::endl;
 		Span span(100);
 		try {
 			std::cout << "span is empty" << std::endl;
+			std::cout << "span.shortestSpan(): " << span.shortestSpan() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << RED << e.what() << END << std::endl;
+		}
+		try {
+			span.addNumber(1);
+			std::cout << "span is one element" << std::endl;
 			std::cout << "span.shortestSpan(): " << span.shortestSpan() << std::endl;
 		}
 		catch (std::exception &e)
